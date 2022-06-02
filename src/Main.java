697c04293293d1b2beec3eb36e318b4e9e47baf4
2078Stasyan <2078lsv@gmail.com>
2022-06-02 23:13:20 +0400

    Изменение условия if

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int money = 1100; //сумма пополнения
+        int money = 1001; //сумма пополнения
         int StartCheck = 100; //начальный счёт
         int bonus; //количество начисленных бонусов
         int SummCheck; //итоговая сумма на счету клиента
 
-        if (money < 999) {
+        if (money <= 1000) {
             bonus = 0;
             SummCheck = money + StartCheck;
         } else {
